9029b01f09a188a45638a49d1e2a327b57792264
robbingdog <k3vinmcd@gmail.com>
2016-04-03 19:23:48 -0400

    Update Polarize.php

--- Polarize.php
+++ Polarize.php
@@ -54,6 +54,39 @@
 				Results and page (C) Copyright Polarize Inc.
 			</p>
 			
+			<?php
+			require_once('TwitterAPIExchange.php');
+ 
+			/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "3344215463-XNj7sDb8WaCvjEbErOHTqaCv9HdlHeZcU7The5X",
+    'oauth_access_token_secret' => "4uPiJrFsL0SuWIefaT3RMnPmR684ce29LrWnY35pI1Lkw",
+    'consumer_key' => "60EwKn8kG8sDW45ZcuFJByOmd",
+    'consumer_secret' => "xOk1d9wqPpzTIK68gACMG5lrv2M587ItlHNe3e2j0auVFQ9VBx"
+);
+ 
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+ 
+$requestMethod = "GET";
+ 
+$getfield = '?screen_name=iagdotme&count=20';
+$twitter = new TwitterAPIExchange($settings);
+$string = json_decode($twitter->setGetfield($getfield)
+->buildOauth($url, $requestMethod)
+->performRequest(),$assoc = TRUE);
+if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+foreach($string as $items)
+    {
+        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+        echo "Tweet: ". $items['text']."<br />";
+        echo "Tweeted by: ". $items['user']['name']."<br />";
+        echo "Screen name: ". $items['user']['screen_name']."<br />";
+        echo "Followers: ". $items['user']['followers_count']."<br />";
+        echo "Friends: ". $items['user']['friends_count']."<br />";
+        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+    }
+?>
+			
 
 		</div>
 
