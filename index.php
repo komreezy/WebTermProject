9bf8263cae7d43c72088d0e6123b80dc050a01f4
Kevin McDaniel <k3vinmcd@gmail.com>
2016-04-21 19:37:41 -0400

    to work with polarize.php

--- index.php
+++ index.php
@@ -1,23 +1,34 @@
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Polarize The Web</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 </head>
 <body>
 	<div id="left">
 		<h1>
-			Love	
+			Love
 		</h1>
+		
+		
 	</div>
 	<div id = "right">
 		<h1>
 			Hate
 		</h1>	
+		<?php include 'Polarize.php'; ?> 
+		
 	</div>
 	<form id="searchbox" action="">
 	    <input id="search" type="text" placeholder="Search Topics">
 	    <input id="submit" type="submit" value="Search">
 	</form>
+	
 </body>
+<script src="visual.js"></script>
 </html>
