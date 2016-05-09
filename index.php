99fc112a9975f0ec79307345c14eeda156834486
Komran Ghahremani <gkomran@gmail.com>
2016-05-09 19:26:29 -0400

    Sentiment Analysis

--- index.php
+++ index.php
@@ -107,9 +107,12 @@
 			Love
 		</h1>
 		<?php
+			require_once __DIR__ . '/phpInsight-master/autoload.php';
+			$sentiment = new \PHPInsight\Sentiment();
+
 			if(isset($_POST["search_result"])){
 				$query = $_POST["search_result"];
-				$getfield = '?q="love%20' . $query . '"&result_type=recent&count='.$count.'&filter=safe';
+				$getfield = '?q="love%20' . $query . '"&result_type=recent&count='.$count.'filter=safe';
 				$twitter = new TwitterAPIExchange($settings);
 				$string = $twitter->setGetfield($getfield)
 				    ->buildOauth($url, $requestMethod)
@@ -118,16 +121,24 @@
 				$result = json_decode($string, true); 
 				foreach($result['statuses'] as $items)
 				{
-					echo "<div class='match'>";
-					echo "Tweet: ". $items['text']."<br />";
-					echo "Time and Date of Tweet: ".$items['created_at']."<br />";
-					echo "Tweeted by: ". $items['user']['name']."<br />";
-					echo "Screen name: ". $items['user']['screen_name']."<br />";
-					echo "Followers: ". $items['user']['followers_count']."<br />";
-					echo "Friends: ". $items['user']['friends_count']."<br />";
-					echo "Listed: ". $items['user']['listed_count']."<br />";
-					echo "</div>";
-					
+					// calculations:
+					$scores = $sentiment->score($items['text']);
+					$class = $sentiment->categorise($items['text']);
+
+					if($scores['pos'] > 0.5) {
+						echo "<div class='match'>";
+						echo "Tweet: ". $items['text']."<br />";
+						//to see scores uncomment
+						//echo "Dominant:". $class.", scores: ";
+						//print_r($scores);
+						echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+						echo "Tweeted by: ". $items['user']['name']."<br />";
+						echo "Screen name: ". $items['user']['screen_name']."<br />";
+						echo "Followers: ". $items['user']['followers_count']."<br />";
+						echo "Friends: ". $items['user']['friends_count']."<br />";
+						echo "Listed: ". $items['user']['listed_count']."<br>";
+						echo "</div>";
+					}
 				}
 			}
 		?>
@@ -138,6 +149,9 @@
 			Hate
 		</h1>	
 		<?php 
+			require_once __DIR__ . '/phpInsight-master/autoload.php';
+			$sentiment = new \PHPInsight\Sentiment();
+
 			if(isset($_POST["search_result"])){
 				$query = $_POST["search_result"];
 				$getfield = '?q="hate%20' . $query . '"&result_type=recent&count='.$count.'filter=safe';
@@ -149,16 +163,24 @@
 				$result = json_decode($string, true); 
 				foreach($result['statuses'] as $items)
 				{
-					echo "<div class='match'>";
-					echo "Tweet: ". $items['text']."<br />";
-					echo "Time and Date of Tweet: ".$items['created_at']."<br />";
-					echo "Tweeted by: ". $items['user']['name']."<br />";
-					echo "Screen name: ". $items['user']['screen_name']."<br />";
-					echo "Followers: ". $items['user']['followers_count']."<br />";
-					echo "Friends: ". $items['user']['friends_count']."<br />";
-					echo "Listed: ". $items['user']['listed_count']."<br>";
-					echo "</div>";
-					
+					// calculations:
+					$scores = $sentiment->score($items['text']);
+					$class = $sentiment->categorise($items['text']);
+
+					if($scores['neg'] > 0.5) {
+						echo "<div class='match'>";
+						echo "Tweet: ". $items['text']."<br />";
+						//to see scores uncomment
+						//echo "Dominant:". $class.", scores: ";
+						//print_r($scores);
+						echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+						echo "Tweeted by: ". $items['user']['name']."<br />";
+						echo "Screen name: ". $items['user']['screen_name']."<br />";
+						echo "Followers: ". $items['user']['followers_count']."<br />";
+						echo "Friends: ". $items['user']['friends_count']."<br />";
+						echo "Listed: ". $items['user']['listed_count']."<br>";
+						echo "</div>";
+					}
 				}
 			}
 
